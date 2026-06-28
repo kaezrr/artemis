3048a793c855fe7427ad9a33dd909af081f60989
Anjishnu Banerjee <kaezr.dev@gmail.com>
2026-06-28 22:07:01 +0530

    add query type

---
+++ src/query.rs
@@ -0,0 +1,50 @@
+use time::Duration;
+
+use crate::media::MediaKind;
+use crate::media::Status;
+use crate::media::Tag;
+
+#[derive(Default)]
+pub struct LibraryQuery {
+    pub search: Option<String>,
+    pub kind: Option<Vec<MediaKind>>,
+
+    pub sort_by: SortBy,
+    pub order: SortOrder,
+
+    pub status: Option<Status>,
+    pub tag_filter: Option<TagFilter>,
+
+    pub limit: Option<u32>,
+    pub offset: Option<u32>,
+}
+
+#[derive(Default)]
+pub enum SortOrder {
+    #[default]
+    Ascending,
+    Descending,
+}
+
+#[derive(Default)]
+pub enum SortBy {
+    #[default]
+    Title,
+    Rating,
+    ReleaseYear,
+    LastModified,
+}
+
+pub enum TagFilter {
+    Or(Vec<Tag>),
+    And(Vec<Tag>),
+}
+
+/// Used to update a library entry
+#[derive(Default)]
+pub struct UpdateEntry {
+    pub status: Option<Status>,
+    pub notes: Option<Option<String>>,
+    pub rating: Option<Option<u8>>,
+    pub playtime: Option<Option<Duration>>,
+}
